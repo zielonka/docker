c11b560560af755e290ad461feca9e7e13e854b0
zielonka <patrick.zielonka@tuta.io>
2016-09-02 23:19:22 -0700

    commit

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ "${AUTHORIZED_KEYS}" != "**None**" ]; then
+    echo "=> Found authorized keys"
+    mkdir -p /root/.ssh
+    chmod 700 /root/.ssh
+    touch /root/.ssh/authorized_keys
+    chmod 600 /root/.ssh/authorized_keys
+    IFS=$'\n'
+    arr=$(echo ${AUTHORIZED_KEYS} | tr "," "\n")
+    for x in $arr
+    do
+        x=$(echo $x |sed -e 's/^ *//' -e 's/ *$//')
+        cat /root/.ssh/authorized_keys | grep "$x" >/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            echo "=> Adding public key to /root/.ssh/authorized_keys: $x"
+            echo "$x" >> /root/.ssh/authorized_keys
+        fi
+    done
+fi
+
+if [ ! -f /.root_pw_set ]; then
+	/set_root_pw.sh
+fi
+
+# If a pidfile is still around (for example after a container restart),
+# delete it so that docker can start.
+rm -rf /var/run/docker.pid
+
+#docker -d
+
+#exec /usr/sbin/sshd -D
+/usr/bin/supervisord
